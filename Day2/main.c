f7297f69f33731e65e9d74fcba4ab5d969346653
Jan K. <kleinjan826@gmail.com>
2024-12-09 17:05:07 +0100

    corrected right Advent Date

--- Day2/main.c
+++ Day2/main.c
@@ -1,45 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
 
-// heufigkeit
-int zaehleHaeufigkeit(int Liste[], int size, int wert) {
-    int zaehler = 0;
-    for (int i = 0; i < size; i++) {
-        if (Liste[i] == wert) {
-            zaehler++;
-        }
+bool ist_sicher(int report[],int laenge){
+    bool steigende_pegel=true,fallende_pegel=true,passende_differenzen=true;
+    for(int i=0;i<laenge-1;i++){
+        int diff=abs(report[i]-report[i+1]);
+        if(diff<1||diff>3) passende_differenzen=false;
+        if(report[i]>=report[i+1]) steigende_pegel=false;
+        if(report[i]<=report[i+1]) fallende_pegel=false;
     }
-    return zaehler;
+    return (steigende_pegel||fallende_pegel)&&passende_differenzen;
 }
 
-// aehnlichkeitswert
-int berechneAehnlichkeitswert(int Liste1[], int Liste2[], int size1, int size2) {
-    int aehnlichkeitswert = 0;
-
-    for (int i = 0; i < size1; i++) {
-        int wert = Liste1[i];
-        int haeufigkeit = zaehleHaeufigkeit(Liste2, size2, wert);
-        aehnlichkeitswert += wert * haeufigkeit;
+int parse_zeile(char *zeile,int report[]){
+    int index=0;
+    char *token=strtok(zeile," ");
+    while(token!=NULL){
+        report[index++]=atoi(token);
+        token=strtok(NULL," ");
     }
-
-    return aehnlichkeitswert;
+    return index;
 }
 
-int main() {
-    FILE *file = fopen("../Day1/listen.txt", "r");
-
-    int Liste1[1000], Liste2[1000];
-    int size1 = 0, size2 = 0;
-
-    while (fscanf(file, "%d %d", &Liste1[size1], &Liste2[size2]) == 2) {
-        size1++;
-        size2++;
+int main(){
+    FILE *datei=fopen("Berichte.txt","r");
+    if(datei==NULL) return 1;
+    char zeile[256];
+    int report[100],anzahl_sicher=0;
+    while(fgets(zeile,sizeof(zeile),datei)){
+        zeile[strcspn(zeile,"\n")]='\0';
+        if(ist_sicher(report,parse_zeile(zeile,report))) anzahl_sicher++;
     }
-
-    fclose(file);
-
-    int aehnlichkeitswert = berechneAehnlichkeitswert(Liste1, Liste2, size1, size2);
-    printf("Der Aehnlichkeitswert ist: %d\n", aehnlichkeitswert);
-
+    fclose(datei);
+    printf("Anzahl der sicheren Berichte:%d\n",anzahl_sicher);
     return 0;
 }
